1b43c4a1dcefe8f20d871ab07c7ab7411fcc26c3
Kaiyi Chang <kaiyi@mainsailnetwork.org>
2022-10-07 20:17:10 -0400

    add demo

--- content/src_verify.py
+++ content/src_verify.py
@@ -1,32 +1,195 @@
+# Note that for testing, the files config.toml and backup.toml 
+#    are written and read from the current dir, which is 
+#    p = Path.home() / "Library/Gennaker/projects/Quick Start" / fname
+# Comment out line 257 and uncomment the line above to undo this. 
+# 
+
 from pathlib import Path
-import shutil
+import base64
+
+from cryptography.hazmat.primitives.asymmetric import ed25519
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
+
 import tomli
 import requests
-import ipywidgets as widgets
 
+import ipywidgets as widgets
 from IPython.display import display
 
-from signing import VerifyFiles, Config
+class Edsig:
+    """Conversions between formats 
+    Let b be a bytearray of any length
+    Let b64 be a corresponding array encoded using base64
 
-#TODO: do we need to add ipywigets to gpython?
+    Let M stand for either 44 or 88
+        b_M is a bytearray of length M 
+        s_M is a string of length M 
+    Let n stand for either 32 or 64 
+        b_n is a bytearray of length n 
 
-# for loop scan for files, create list of vf, list of pf
-# for loop create a list of radio btn and a list of checkbox widget
-# display the widgets
-# for loop check result with index of list of vf
-# need to pass down lw, lw2, lvf. shallow copies?
+    Hence, b64_M is a base64 encoded bytearray of length M 
 
-# single authenticate objects, functions
+    A key can be represented as 
+        b_32: bytes len = 32 with default encoding
+        b64_44: base64 encoded bytearray of len = 44 
+        s_44: string len = 44
+
+    A signature can be represented as 
+        b_64: bytes len = 64 with default encoding 
+        b64_88: base64 encoded bytesarray of len = 88
+        s_88: string len = 88
+
+    To convert, use:
+        b_2_s: b_n to s_M
+        s_2_b: s_M to b_n 
+
+    """
+    @staticmethod
+    def bundle_suffix():
+        return '.edbnl'
+    
+    @staticmethod
+    def signature_suffix():
+        return '.edsig'
+    
+    @staticmethod
+    def s_2_b(s):
+        return base64.urlsafe_b64decode(bytes(s, 'utf-8'))
+        
+    @staticmethod
+    def b_2_s(b):
+        return str(base64.urlsafe_b64encode(b), 'utf-8')
+
+    @staticmethod
+    def blk_2_sig(b: bytes)->str:
+        """Reads a 315 byte block and returns a signature string."""
+        blk = b[:315]
+        s = str(blk, 'utf-8')
+        sig = s[45:89] + s[90:134] 
+        return sig
+    
+    @staticmethod
+    def blk_2_ps(b: bytes)->str:
+        """Reads a 315 byte block and returns a public string."""
+        blk = b[:315]
+        s = str(blk, 'utf-8')
+        ps = s[225:269]
+        return ps
+
+    @staticmethod
+    def bytes_2_msg(b: bytes) -> bytes:
+        """Extract the msg bytes from a bundle"""
+        return b[315:]
+    
+    @staticmethod 
+    def sig_ps_2_blk(sig, ps):
+        """Converts a sig string and public string to a 315 byte block."""
+        s1 = ("BEGIN SIGNATURE".center(44, "-") + "\n"
+            + sig[:44] + "\n"
+            + sig[44:] + "\n"
+            + "END SIGNATURE".center(44, "-") + "\n")
+        s2 = ("BEGIN PUBLIC KEY FOR SIGNER".center(44, "-") + "\n"
+            + ps + "\n"
+            + "END PUBLIC KEY FOR SIGNER".center(44, "-") + "\n")
+        return bytes(s1 + s2, 'utf-8')
+
+
+class VerifyFiles(Edsig):
+    """Takes key value arguments for the three file locations. 
+    Accepts any one of them, or both p_doc and p_sig, which can then 
+    be in different directories. 
+    """
+    def __init__(
+        self, p_doc: Path = None, p_sig: Path = None, 
+        p_bndl: Path = None
+        ):
+        if not p_bndl and not p_sig and not p_doc:
+            print("Error: You need to supply at least one file path.")
+            # return False 
+
+            # problem: cannot return anything other than None from init
+            # ^returning False also does not abort object instance initialization
+            #REVIEW: As a special constraint on constructors, 
+            # no value may be returned; doing so will cause a TypeError to be raised at runtime.
+            # https://docs.python.org/3/reference/datamodel.html#object.__init__
+        elif p_bndl and p_sig: 
+            print("Error: Two locations for the signature file.")
+            # return False
+        elif p_bndl and p_doc: 
+            print("Error: Two locations for the msg.")
+            # return False
+
+        self.p_doc = p_doc
+        self.p_bndl = p_bndl
+        self.p_sig = p_sig
+
+        #CHANGE: set these attributes to None first so they all exist, 
+        # catch the None in auto-verify
+        self.sig = None
+        self.ps = None
+        self.msg = None
+
+        # Read the values for verifying
+        if self.p_bndl and self.p_bndl.is_file():
+            b = self.p_bndl.read_bytes()
+            self.sig = self.blk_2_sig(b)
+            self.ps = self.blk_2_ps(b)
+            self.msg = self.bytes_2_msg(b)
+            self.p_doc = self.p_bndl.parent / (self.p_bndl.stem)
+        else: 
+            if self.p_doc and not self.p_sig:
+                self.p_sig = Path(str(self.p_doc) + self.signature_suffix()) 
+            elif self.p_sig and not self.p_doc:
+                self.p_doc = self.p_sig.parent / (self.p_sig.stem)
+            if self.p_sig.is_file():
+                b = self.p_sig.read_bytes()
+                self.sig = self.blk_2_sig(b)
+                self.ps = self.blk_2_ps(b)
+            else:
+                print(f"The signature file does not exist at: {self.p_sig}\n")
+                # return False
+            if self.p_doc.is_file():
+                self.msg = self.p_doc.read_bytes()
+            else:
+                print(f"The file does not exist at: {self.p_doc}\n")
+                # return False
+             
+             
+class PublicObject(VerifyFiles, Edsig):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __str__(self):
+        """Returns the string from the corresponding NaCl object."""
+        return self.public_string
+
+    def __bytes__(self):
+        """Returns the bytes from the corresponding NaCl object."""
+        return self.s_2_b(self.public_string)
+    
+
+    def verify(self) -> bool:
+        try:
+            msg = ed25519.Ed25519PublicKey.from_public_bytes(self.s_2_b(self.ps)).verify(self.s_2_b(self.sig), self.msg)
+            # if self.p_bndl and self.p_bndl.is_file():
+            #     self.p_doc.write_bytes(msg)
+            return True
+        except InvalidSignature:
+            return False
+
 
 class Authenticate():
-    def __init__(self):
-        t = self.scan_and_display()
+    def __init__(self, verified_dir=None):
+    
+        t = self.scan_and_display(Path(verified_dir))
+
         self.l_vf = t[0]
         self.l_rdbns = t[1]
 
-    def scan_and_display(self):
+    def scan_and_display(self, verified_dir):
         
-        l_vf = self.scan_for_files()
+        l_vf = self.scan_for_files(verified_dir)
 
         # Create 2 lists of widgets
         l_doc_w = self.l_docname_html(l_vf)
@@ -69,13 +232,13 @@ class Authenticate():
         return (l_vf, l_rdbns_w)
 
     @staticmethod
-    def scan_for_files() -> VerifyFiles:
+    def scan_for_files(verified_dir=None) -> VerifyFiles:
         """Scan for edsig and edbnl files in destination folder. Only 
         look for edsig or edbnl, because only these contain public key"""
         
         l = list()
-
-        verified_dir=Config().fdict["vout"]
+        if not verified_dir:
+            verified_dir=Path("./")
 
         for p in verified_dir.iterdir():
             
@@ -106,31 +269,20 @@ class Authenticate():
             
     def check_results(self):
         # Radio buttons: move files to folders 
-        c = Config()
         for i, bns in enumerate(self.l_rdbns):
 
             vf = self.l_vf[i] # The corresponding vf obj
 
             # Move file to quarantine if verification is not successful
             if bns.value == "Do not trust sender":
-                p_dst = c.fdict["qrt"]
-
+                print("Do not trust sender")
             elif bns.value == "Trust only for current doc":
-                p_dst = c.fdict["cout"]
+                print("Trust only for current doc")
             
             elif bns.value == "Add to trusted sender list":
-                p_dst = c.fdict["cout"]
-
-                #TODO: check duplicate public key?
-                with c.trusted.open('a') as fp:
-                    fp.write(vf.ps + "\n")
+                print("Add to trusted sender list")
             
-            if vf.p_doc.is_file():
-                shutil.move(vf.p_doc, p_dst / vf.p_doc.name) # REVIEW: depends at which step file is extracted
-            if vf.p_bndl:
-                shutil.move(vf.p_bndl, p_dst / vf.p_bndl.name)
-            elif vf.p_sig:
-                shutil.move(vf.p_sig, p_dst / vf.p_sig.name)
+
 
     @staticmethod
     def xHTML(value):
@@ -162,7 +314,7 @@ class Authenticate():
                         #     {vf.p_doc.name}
                         # </div>
                         # """,#font-size: 1rem;15rem
-                    layout=widgets.Layout(height='auto', width='auto', margin="0 10px 0 0", border="thin solid")
+                    layout=widgets.Layout(height='auto', width='auto', margin="0 10px 0 0",)
                     # value=f"<div style='overflow: hidden; '>{vf.p_doc.name}</div>", #text-overflow: ellipsis;
                     # layout=widgets.Layout(height='auto', width='auto', border='solid')
                 )
@@ -170,7 +322,7 @@ class Authenticate():
         return l
 
     @staticmethod
-    def fetch_profile_from_servers(vf) -> tuple[bool, str]:
+    def fetch_profile_from_servers(vf):# -> tuple[bool, str]:
         """Find a member profile associated with a public key, if active 
         member profiles are found on > 2 of the servers, return (True, profile), 
         otherwise return (False, message)"""
@@ -298,8 +450,9 @@ class Authenticate():
                 layout=widgets.Layout(
                     overflow='hidden',
                     width='100%',
+                    height='auto',
                     grid_template_rows='auto', # 1 row of auto width
-                    grid_template_columns='30% 40% 30%', # 4 columns
+                    grid_template_columns='33.3% 33.3% 33.3%', # 4 columns
                 )
             )
             l.append(rg)
@@ -337,6 +490,46 @@ class Authenticate():
                         ),
                         right_sidebar=None,
                         footer=None,
-                        pane_heights = [1, 6, 0],
+                        pane_heights = [1, 2, 0],
                         layout = widgets.Layout(margin = '2em')
                         )
+
+# auto-verify
+# scan for all files in input dir, store in list
+# test file ext, pass the corresponding kwarg based on ext to initialize PublicObject
+# po.verify(), #DECIDE:method extracts file if bundle), catch the bool here
+# check which output path exist, and move either p_doc and p_bndl or p_doc and p_sig
+# move to quarantine if check failed, move to verified if check successful
+#CHANGE
+
+# ==== demo ====
+
+# Verify bundle
+def verify_bundle(p_bundle: str):
+    if Path(p_bundle).suffix == ".edbnl":
+        po = PublicObject(p_bndl=Path(p_bundle))
+        if not po:
+            print("Could not perform verification operation.")
+            return False
+        else:
+            if po.verify():
+                print("The signature and message verify for the bundle:")
+                print(po.p_bndl.resolve())
+                print()
+                # print("The document has been saved to:")
+                # print(po.p_doc.resolve())
+                return True
+            else:
+                print("The signature and file are not consistent.")
+                print("You should not trust the document.")
+                return False
+ 
+    elif Path(p_bundle).suffix == ".edsig":
+        print("This is the wrong file format for this function.")
+        print("Use the verify_signatrue() function.")
+        return False
+    else:
+        print("This is the wrong file format for this function.")
+        print("Use the verify_document() function.")
+        return False
+        
